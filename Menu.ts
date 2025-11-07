97f08947e7cf3e504a949a8c393848e7c79a4e32
Lancellot <assis.neto@rede.ulbra.br>
2025-11-07 17:37:11 -0300

    feat: adição do modulo conta

--- Menu.ts
+++ Menu.ts
@@ -1,14 +1,22 @@
 import readlinesync from 'readline-sync';
 import { colors } from './src/util/Colors.ts';
+import { Conta } from './src/model/Conta.ts';
 
 export function main() {
 
     let opcao: number;
 
+    const conta: Conta = new Conta(1, 123, 1, 'Adriana', 10000);
+    conta.visualizar();
+    conta.sacar(500);
+    conta.visualizar();
+    conta.depositar(130);
+    conta.visualizar();
+
     while (true) {
 
-        console.log(colors.bg.black, colors.fg.yellow, 
-                    "*****************************************************");
+        console.log(colors.bg.black, colors.fg.redstrong);
+        console.log("*****************************************************");
         console.log("                                                     ");
         console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
