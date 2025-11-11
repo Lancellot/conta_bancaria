c0ae32f8b8d3ab9a3f162df21b7d7a9e83c54c3c
Lancellot <assis.neto@rede.ulbra.br>
2025-11-11 14:50:19 -0300

    refactor: transformando a class conta em abstrata

--- Menu.ts
+++ Menu.ts
@@ -8,27 +8,6 @@ export function main() {
 
     let opcao: number;
 
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
-
-    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
-    contacorrente.visualizar();
-    contacorrente.sacar(2000);
-    contacorrente.visualizar();
-    contacorrente.depositar(1000);
-    contacorrente.visualizar();
-
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
-
     while (true) {
 
         console.log(colors.bg.black, colors.fg.redstrong);
